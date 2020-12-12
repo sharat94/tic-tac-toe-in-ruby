a821f2ce844af1377cba06f7a0caca280c057156
hushimatata <sharat.oommen@finly.io>
2020-12-12 23:40:13 +0530

    Board with print functionality addded

--- lib/board.rb
+++ lib/board.rb
@@ -1,3 +1,22 @@
 class Board
 
+  def initialize(grid_length: 3)
+    @grid_length = grid_length.to_i
+    @grid = []
+    initialize_grid
+  end
+
+  def print
+    @grid.each_with_index do |row, index|
+      puts row.join(',').gsub(',','|')
+      puts '- - -' unless index == (@grid_length - 1)
+    end
+  end
+
+  private
+  def initialize_grid
+    @grid_length.times.each do |x|
+      @grid.push(Array.new(@grid_length, ' '))
+    end
+  end
 end
